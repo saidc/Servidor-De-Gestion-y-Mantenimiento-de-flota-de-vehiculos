a3edeb3cd6401954cb5ecc71775057802b8387bf
saidjoc@gmail.com <saidjoc@gmail.com>
2021-10-07 00:34:44 -0500

    agregando query de consulta

--- conexion.js
+++ conexion.js
@@ -15,6 +15,13 @@ conexion.connect(function(error){
         console.log("conexion exitosa");
     }
 });
+conexion.query("SELECT * FROM heroku_d887aadfd8b0128.usuario; ",(error, results, fields) =>{
+    if(error){
+        throw error;
+    }
+    console.log(results);
+});
 
 // finaliza la conexion
 conexion.end();
+// 2C3710DEB86A594120A43185687A70081AE9AB293C6100C8F8C94AA155C3B5DF
