86d5b2ef0687b0e6fa81a91fea3145714e1dd137
saidjoc@gmail.com <saidjoc@gmail.com>
2021-11-26 10:00:02 -0500

    update

--- public/home/javascript/index.js
+++ public/home/javascript/index.js
@@ -1,5 +1,6 @@
 var host = "http://127.0.0.1:3000";//"https://fast-chamber-47171.herokuapp.com:/"
-
+print(window.location.hostname)
+print(window.location.href)
 var payload = {
     pageNo: 0, count: 10
 };
